c99cddc958bb6e0dabd8b0cc9445fc57314652c2
dmozze <Lacoste0two@yandex.ru>
2018-12-08 09:44:19 +0000

    edit split to remove/add/clear and code auto uploading unsolved tasks

--- longpoll.py
+++ longpoll.py
@@ -8,6 +8,7 @@ import shelve
 
 from config import *
 from externalfunctions import *
+from upload import *
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
@@ -17,7 +18,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 def checkUserName(username):
-    if username.lower() == TEACHER_USERNAME or username.lower() == ADMIN_USERNAME:
+    if username != None and (username.lower() == TEACHER_USERNAME or username.lower() == ADMIN_USERNAME):
         return True
     else:
         return False
@@ -47,10 +48,13 @@ def help(bot, update):
         )
     else:
         update.message.reply_text(
-            '/reg `Фамилия` зарегистрироваться под `Фамилией`\n'
-            '/edit принимает ваши задачи через пробел, если задачи не было, он ее добавит, иначе исключит из списка задач на следующую практику\n'
-            '/show показывает, какие задачи заявлены на следующую практику\n'
-            '/csv - In development'
+            '/reg `Фамилия` - зарегистрироваться под `Фамилией`\n'
+            '/add `Номера задач через пробел` - добавить задачи\n'
+            '/remove `Номера задач через пробел` - удалить задачи\n'
+            '/add и /remove поддерживают ввод отрезками\n'
+            'Пример: /add 125 126-129 126-130 - вам будут добавлены задачи [125 .. 130]\n'
+            '/clear - очистит список ваших задач\n'
+            '/show - показывает список ваших задач\n'
             'Отзывы и предложения сюда: `@Dmozze`',
             parse_mode='Markdown'
         )
@@ -66,7 +70,7 @@ def checkIn(bot, update, args):
             with shelve.open('names_db') as shelve_names:
                 user_id = str(update.message.chat_id)
                 if user_id in shelve_names:
-                    update.message.reply_text('Вы уже зарегистрированы как ' + i)
+                    update.message.reply_text('Вы уже зарегистрированы как ' + shelve_names[user_id]['name'])
                     return
                 shelve_names[user_id] = {'name' : i, 'tasks' : []}
                 update.message.reply_text('Вы были удачно зарегистрированны')
@@ -79,28 +83,46 @@ def is_correct_value(value):
     else:
         return False
 
-def edit(bot, update, args):
+def clear(bot, update):
     with shelve.open('names_db',writeback=True) as shelve_names:
         user_id = str(update.message.chat_id)
         if user_id not in shelve_names:
             update.message.reply_text('Вы не зарегистрированы, смотрите /help')
             return
-        if len(args) == 0:
-            shelve_names[user_id]['tasks'] =  []
-            update.message.reply_text('Список ваших задач пуст')
-            return
-        invalues = set()
-        for value in args:
-            if is_correct_value(value):
-                invalues.add(int(value))
-            else:
-                 rng = value.split('-')
-                 if len(rng) == 2 and is_correct_value(rng[0]) and is_correct_value(rng[1]):
-                     for number in range(int(rng[0]), int(rng[1]) + 1):
-                         invalues.add(number)
-        shelve_names[user_id]['tasks'] =  sorted(list(set(shelve_names[user_id]['tasks']) ^ invalues))
+        shelve_names[user_id]['tasks'] = []
+    show(bot,update)
 
-    show(bot, update)
+def good_set(array):
+    invalues = set()
+    for value in array:
+        if is_correct_value(value):
+            invalues.add(int(value))
+        else:
+            rng = value.split('-')
+            if len(rng) == 2 and is_correct_value(rng[0]) and is_correct_value(rng[1]):
+                for number in range(int(rng[0]), int(rng[1]) + 1):
+                    invalues.add(number)
+    return invalues
+
+def add(bot, update,args):
+    with shelve.open('names_db',writeback=True) as shelve_names:
+        user_id = str(update.message.chat_id)
+        if user_id not in shelve_names:
+            update.message.reply_text('Вы не зарегистрированы, смотрите /help')
+            return
+        toadd = good_set(args)
+        shelve_names[user_id]['tasks'] = sorted(list(set(shelve_names[user_id]['tasks']) | toadd))
+    show(bot,update)
+
+def remove(bot, update,args):
+    with shelve.open('names_db',writeback=True) as shelve_names:
+        user_id = str(update.message.chat_id)
+        if user_id not in shelve_names:
+            update.message.reply_text('Вы не зарегистрированы, смотрите /help')
+            return
+        toremove = good_set(args)
+        shelve_names[user_id]['tasks'] = sorted(list(set(shelve_names[user_id]['tasks']) - toremove))
+    show(bot,update)
 
 def show(bot,update):
     with shelve.open('names_db') as shelve_names:
@@ -109,7 +131,7 @@ def show(bot,update):
             update.message.reply_text('Вы не зарегистрированы, смотрите /help')
             return
         if len(shelve_names[user_id]['tasks']) == 0:
-            update.message.reply_text('Вы не зарегистрированы, смотрите /help')
+            update.message.reply_text('Список ваших задач пуст')
         else:
             update.message.reply_text('Вы заявили задачи под номерами: ' + ', '.join([str(i) for i in shelve_names[user_id]['tasks']]))
 
@@ -117,14 +139,13 @@ def update(bot,update):
     if not checkUserName(update.message.from_user.username):
         update.message.reply_text('У вас недостаточно прав для выполнения указанной команды')
         return
-    unsolvedtasks = update_tasks()
+    unsolvedtasks = upload()
     data = []
     with shelve.open('names_db') as shelve_names:
         for tasknumber in unsolvedtasks:
             solvedby = [tasknumber]
             for id in shelve_names.keys():
-                ## FIXME:Сделать интовые задачи
-                if int(tasknumber) in shelve_names[id]['tasks']:
+                if tasknumber in shelve_names[id]['tasks']:
                     solvedby.append(shelve_names[id]['name'])
             if len(solvedby) > 1:
                 data.append(solvedby)
@@ -143,10 +164,11 @@ if __name__ == '__main__':
     dp.add_handler(CommandHandler('start', start))
     dp.add_handler(CommandHandler('help', help))
     dp.add_handler(CommandHandler('reg', checkIn, pass_args=True))
-    dp.add_handler(CommandHandler('edit', edit, pass_args=True))
     dp.add_handler(CommandHandler('show', show))
     #dp.add_handler(CommandHandler('csv', csv))
-
+    dp.add_handler(CommandHandler('clear', clear))
+    dp.add_handler(CommandHandler('remove', remove, pass_args=True))
+    dp.add_handler(CommandHandler('add', add, pass_args=True))
     #privatehandlers
     dp.add_handler(CommandHandler('update', update))
     dp.add_handler(CommandHandler('debug', debug))
